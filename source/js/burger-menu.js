e77f1d0fd86ea97bfa232ce51564a03f0646c0cf
Лиана <verbovskayaliana@gmail.com>
2025-06-30 13:37:29 +0300

    правки

---
+++ source/js/burger-menu.js
@@ -0,0 +1,7 @@
+// const btn = document.querySelector('.header__button');
+// // const list = document.querySelector('.header__list');
+
+// btn.addEventListener('click', () => {
+//   list.style.display === 'flex' ? list.style.display = 'none' : list.style.display = 'flex';
+//   btn.classList.toggle('active');
+// });
